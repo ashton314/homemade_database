29d3af70a8fb5411a72080575b4ada8d0b651d6d
Ashton Wiersdorf <ashton.wiersdorf@mailblock.net>
2016-12-10 05:31:06 +0000

    BROKEN - identified a bug when the first child of the root splits

--- t.pl
+++ t.pl
@@ -21,14 +21,37 @@ $tree0->insert('a');
 is_deeply($tree0->{head_node}->{keys}, ['a', 'b'], 'keys inserted a, b correct');
 is_deeply($tree0->{head_node}->{values}, ['a', 'b'], 'values inserted a, b correct');
 
-$tree0->insert('c');
+$tree0->insert('d');
 
 is_deeply($tree0->to_hash, { keys   => ['b'],
 			     values => [ { keys => ['a'], values => ['a'] },
-					 { keys => ['b', 'c'], values => ['b', 'c']} ] },
-	  'inserted c and split correctly');
+					 { keys => ['b', 'd'], values => ['b', 'd']} ] },
+	  'inserted d and split correctly');
 
-$tree0->insert('d');
+$tree0->insert('c');
+is_deeply($tree0->to_hash, { keys   => ['b', 'd'],
+			     values => [ { keys => ['a'], values => ['a'] },
+					 { keys => ['b', 'c'], values => ['b', 'c']},
+					 { keys => ['d'], values => ['d'] } ] },
+	  'inserted c correctly, split correctly again');
+
+$tree0->insert('aa');
+is_deeply($tree0->to_hash, { keys   => ['b', 'd'],
+			     values => [ { keys => ['a', 'aa'], values => ['a', 'aa'] },
+					 { keys => ['b', 'c'], values => ['b', 'c']},
+					 { keys => ['d'], values => ['d'] } ] },
+	  'inserted aa correctly');
+
 print Dumper($tree0->to_hash);
 
+print "-" x 80; print "\n";
+
+$tree0->insert('ab');
+# is_deeply($tree0->to_hash, { keys   => ['b', 'd'],
+# 			     values => [ { keys => ['a', 'aa'], values => ['a', 'aa'] },
+# 					 { keys => ['b', 'c'], values => ['b', 'c']},
+# 					 { keys => ['d'], values => ['d'] } ] },
+# 	  'inserted ab correctly');
+
+print Dumper($tree0->to_hash);
 done_testing();
